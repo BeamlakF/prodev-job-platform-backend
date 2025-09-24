dc4750c8e19f9d81ee025ec613658fb9c856a371
BeamlakF <beamlakfekadu562@gmail.com>
2025-09-24 22:58:45 +0300

    serializer, viewset

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, CategoryViewSet, JobViewSet, ApplicationViewSet
+
+router = DefaultRouter()
+router.register('users', UserViewSet)
+router.register('categories', CategoryViewSet)
+router.register('jobs', JobViewSet)
+router.register('applications', ApplicationViewSet)
 
 urlpatterns = [
-    
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import User, Category, Job, Application
+from .serializers import UserSerializer, CategorySerializer, JobSerializer, ApplicationSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]  # Only admins can list/manage users
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class JobViewSet(viewsets.ModelViewSet):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class ApplicationViewSet(viewsets.ModelViewSet):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
+    permission_classes = [permissions.IsAuthenticated]
