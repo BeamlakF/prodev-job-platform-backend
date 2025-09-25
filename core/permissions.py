575c0a9bb005b82244bc0f81f087e3dd4ea3b502
BeamlakF <beamlakfekadu562@gmail.com>
2025-09-25 12:59:27 +0300

    permissions-Role Based Access

---
+++ core/permissions.py
@@ -0,0 +1,21 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+class IsAdmin(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'admin'
+
+
+class IsUser(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'user'
+
+
+class IsOwnerOrReadOnly(BasePermission):
+    """
+    Allow users to edit only their own objects.
+    """
+    def has_object_permission(self, request, view, obj):
+        # SAFE_METHODS = GET, HEAD, OPTIONS → read-only allowed
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.user == request.user
